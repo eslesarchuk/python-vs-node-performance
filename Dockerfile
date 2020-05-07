fc26349080219206005b5938de391c694b7407a3
Eugene Slesarchuk <eslesarchuk@gmail.com>
2020-05-07 21:23:13 +0300

    sanic with 4 workers, nginx for comparison, lesser requests

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM debian:buster
 WORKDIR /app
-RUN apt-get update && apt-get install -y procps apache2-utils python3.7 python3-pip
+RUN apt-get update && apt-get install -y nginx bc procps apache2-utils python3.7 python3-pip
 RUN apt-get install -y curl && curl -sL https://deb.nodesource.com/setup_14.x | bash - && apt-get install -y nodejs
 
 ADD python/requirements.txt /app/python/
@@ -10,5 +10,6 @@ ADD node/package.json node/package-lock.json /app/node/
 RUN cd node && npm install
 
 ADD . /app
+ADD nginx/default /etc/nginx/sites-enabled/
 
 CMD ["/app/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,26 +3,38 @@ setsid node/run_express.js &
 setsid node/run_fastify.js &
 setsid python/run_tornado.py  &
 setsid python/run_sanic.py 2>&1 1>/dev/null &
+setsid python/run_sanic_4w.py 2>&1 1>/dev/null &
+nginx
 
 sleep 3
 
 echo -e "\n$(tput setaf 2)testing -- node $(node -v), express framework $(npm info express version)$(tput sgr0)"
-ab -c200 -n20000 127.0.0.1:8002/ 2>&1 | egrep "(Concurrency Level|Complete requests|Requests per second)" | while read line; do
+ab -c50 -n10000 127.0.0.1:8002/ 2>&1 | egrep "(Concurrency Level|Complete requests|Requests per second|timeout)" | while read line; do
   echo "$(tput setaf 7)$line$(tput sgr0)"
 done
 
 echo -e "\n$(tput setaf 2)testing -- node $(node -v), fastify framework $(npm info fastify version)$(tput sgr0)"
-ab -c200 -n20000 127.0.0.1:8003/ 2>&1 | egrep "(Concurrency Level|Complete requests|Requests per second)" | while read line; do
+ab -c50 -n10000 127.0.0.1:8003/ 2>&1 | egrep "(Concurrency Level|Complete requests|Requests per second|timeout)" | while read line; do
   echo "$(tput setaf 7)$line$(tput sgr0)"
 done
 
 echo -e "\n$(tput setaf 2)testing -- $(python3 -V), tornado framework $(pip3 show tornado | grep Version | awk '{print $2}')$(tput sgr0)"
-ab -c200 -n20000 127.0.0.1:8004/ 2>&1 | egrep "(Concurrency Level|Complete requests|Requests per second)" | while read line; do
+ab -c50 -n10000 127.0.0.1:8004/ 2>&1 | egrep "(Concurrency Level|Complete requests|Requests per second|timeout)" | while read line; do
   echo "$(tput setaf 7)$line$(tput sgr0)"
 done
 
 echo -e "\n$(tput setaf 2)testing -- $(python3 -V), sanic framework $(pip3 show sanic | grep Version | awk '{print $2}')$(tput sgr0)"
-ab -c200 -n20000 127.0.0.1:8001/ 2>&1 | egrep "(Concurrency Level|Complete requests|Requests per second)" | while read line; do
+ab -c50 -n10000 127.0.0.1:8001/ 2>&1 | egrep "(Concurrency Level|Complete requests|Requests per second|timeout)" | while read line; do
+  echo "$(tput setaf 7)$line$(tput sgr0)"
+done
+
+echo -e "\n$(tput setaf 2)testing -- $(python3 -V), sanic framework $(pip3 show sanic | grep Version | awk '{print $2}'), 4 workers$(tput sgr0)"
+ab -c50 -n10000 127.0.0.1:8005/ 2>&1 | egrep "(Concurrency Level|Complete requests|Requests per second|timeout)" | while read line; do
+  echo "$(tput setaf 7)$line$(tput sgr0)"
+done
+
+echo -e "\n$(tput setaf 2)testing -- $(nginx -v 2>&1 | awk '{print $3}'), workers = cpu cores$(tput sgr0)"
+ab -c50 -n10000 127.0.0.1:8005/ 2>&1 | egrep "(Concurrency Level|Complete requests|Requests per second|timeout)" | while read line; do
   echo "$(tput setaf 7)$line$(tput sgr0)"
 done
 
@@ -31,7 +43,9 @@ tput setaf 7
 echo "$(ps aux | grep express | grep -v grep | awk '{print $4}')% express"
 echo "$(ps aux | grep fastify | grep -v grep | awk '{print $4}')% fastify"
 echo "$(ps aux | grep tornado | grep -v grep | awk '{print $4}')% tornado"
-echo "$(ps aux | grep sanic | grep -v grep | awk '{print $4}')% sanic"
+echo "$(ps aux | grep sanic.py | grep -v grep | awk '{print $4}')% sanic"
+echo "$(echo 0 $(ps aux | grep sanic_4w | grep -v grep | awk '{print $4}' | xargs -n1 echo +) | bc)% sanic (4 workers)"
+echo "$(echo 0 $(ps aux | grep nginx | grep -v grep | awk '{print $4}' | xargs -n1 echo +) | bc)% nginx"
 tput sgr0
 
 echo
